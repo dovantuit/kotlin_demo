81e802e5e2481d8a94e1ffb922d9445b5584106f
Tự Đỗ <dovantuit@gmail.com>
2019-09-12 17:30:59 +0700

    add vcs

--- demo/app/src/main/java/com/example/demo_kotlin/DocumentActivity.kt
+++ demo/app/src/main/java/com/example/demo_kotlin/DocumentActivity.kt
@@ -8,5 +8,24 @@ class DocumentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_document)
+        // xu ly main
+
+        ///
+
     }
+    /// funciton define
+
+    /*
+    // hàm api bình thường
+    fun run(url: String) {
+        val request = Request.Builder()
+            .url(url)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {}
+            override fun onResponse(call: Call, response: Response) = println(response.body()?.string())
+        })
+    }
+    */
 }
--- demo/app/src/main/java/com/example/demo_kotlin/LoginActivity.kt
+++ demo/app/src/main/java/com/example/demo_kotlin/LoginActivity.kt
@@ -14,13 +14,14 @@ class LoginActivity : AppCompatActivity() {
 
         /// xu ly main
         xulyBtnLogIn()
-
+        xulyBtnSignUp()
+        xulyBtnForgotPassWord()
     }
     // onClick nut dang nhap
     fun xulyBtnLogIn(){
         btn_login.setOnClickListener{
             btn_login.setOnClickListener {
-                if(et_password.text.toString() == "1" && et_user.text.toString() == "admin"){
+                if(et_password.text.toString() == "1" && et_user.text.toString() == "1"){
                     val intent : Intent = Intent(this@LoginActivity, DocumentActivity::class.java)
                     startActivities(arrayOf(intent))
                 }
@@ -31,9 +32,15 @@ class LoginActivity : AppCompatActivity() {
     }
     // onClick nut dang ki
     fun xulyBtnSignUp(){
-        tv_signin.setOnClickListener{
-            val intent : Intent = Intent(this@LoginActivity, DocumentActivity::class.java)
-            startActivities(arrayOf(intent))
+        tv_signup.setOnClickListener{
+            Toast.makeText(this, "Tính năng này đang được phát trỉên", Toast.LENGTH_SHORT).show()
+
         }
+    }
+    fun xulyBtnForgotPassWord(){
+        tv_forgotpassword.setOnClickListener{
+            Toast.makeText(this, "Tính năng này đang được phát trỉên", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
